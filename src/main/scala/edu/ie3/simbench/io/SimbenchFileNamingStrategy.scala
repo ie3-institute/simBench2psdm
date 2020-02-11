3c01b874aa3c49571a2a7bfa912c633bc2d18c88
Kittl, Chris <chris.kittl@tu-dortmund.de>
2020-02-11 15:52:16 +0100

    Naming strategy for simbench models

---
+++ src/main/scala/edu/ie3/simbench/io/SimbenchFileNamingStrategy.scala
@@ -0,0 +1,68 @@
+package edu.ie3.simbench.io
+
+import edu.ie3.simbench.exception.io.SimbenchFileNamingException
+import edu.ie3.simbench.model.datamodel.ExternalNet.{Simple, Ward, WardExtended}
+import edu.ie3.simbench.model.datamodel.Line.{ACLine, DCLine}
+import edu.ie3.simbench.model.datamodel.types.LineType.{ACLineType, DCLineType}
+import edu.ie3.simbench.model.datamodel.types.{
+  Transformer2WType,
+  Transformer3WType
+}
+import edu.ie3.simbench.model.datamodel.{
+  Coordinate,
+  Load,
+  Node,
+  RES,
+  SimbenchModel,
+  StudyCase,
+  Transformer2W,
+  Transformer3W
+}
+
+import scala.util.{Failure, Success, Try}
+
+/**
+  * Describes a sophisticated mapping between classes and equivalent SimBench files
+  */
+case object SimbenchFileNamingStrategy {
+  private def modelFileMapping: Map[Class[_ <: SimbenchModel], String] =
+    Map[Class[_ <: SimbenchModel], String](
+      classOf[Coordinate] -> "Coordinates",
+      classOf[Simple] -> "ExternalNet",
+      classOf[Ward] -> "ExternalNet",
+      classOf[WardExtended] -> "ExternalNet",
+      classOf[ACLine] -> "Line",
+      classOf[DCLine] -> "Line",
+      classOf[ACLineType] -> "LineType",
+      classOf[DCLineType] -> "LineType",
+      classOf[Load] -> "Load",
+      classOf[Node] -> "Node",
+      classOf[RES] -> "RES",
+      classOf[StudyCase] -> "StudyCase",
+      classOf[Transformer2W] -> "Transformer",
+      classOf[Transformer2WType] -> "Transformer",
+      classOf[Transformer3W] -> "Transformer3W",
+      classOf[Transformer3WType] -> "Transformer3W"
+    )
+  // TODO: Add naming strategy for profiles + NodePFResult
+
+  /**
+    * Determine the file name based on the provided class
+    *
+    * @param clazz The class to determine the according file name for
+    * @return [[scala.util.Success]], if the file name can be determine, [[scala.util.Failure]], if not
+    */
+  def getFileName(clazz: Class[_]): Try[String] = {
+    if (classOf[SimbenchModel].isAssignableFrom(clazz)) {
+      modelFileMapping.get(clazz.asSubclass(classOf[SimbenchModel])) match {
+        case Some(filename) => Success(filename)
+        case None =>
+          Failure(new SimbenchFileNamingException(
+            s"Cannot determine the filename for class ${clazz.getSimpleName}"))
+      }
+    } else
+      Failure(
+        new SimbenchFileNamingException(
+          s"Cannot determine the filename for class ${clazz.getSimpleName}"))
+  }
+}
