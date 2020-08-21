8d8cb6613b50f962424b46921dee700f1e5beaf7
Kittl, Chris <chris.kittl@tu-dortmund.de>
2020-08-21 16:29:44 +0200

    Adding model for nodal power flow results

---
+++ src/main/scala/edu/ie3/simbench/model/datamodel/NodePFResult.scala
@@ -0,0 +1,110 @@
+package edu.ie3.simbench.model.datamodel
+
+import edu.ie3.simbench.exception.io.SimbenchDataModelException
+import edu.ie3.simbench.io.HeadLineField
+import edu.ie3.simbench.io.HeadLineField.{MandatoryField, OptionalField}
+import edu.ie3.simbench.model.RawModelData
+import edu.ie3.simbench.model.datamodel.EntityModel.EntityModelCompanionObject
+
+/**
+  * Model class for nodal power flow results
+  *
+  * @param node            Reference to the node
+  * @param vm              Voltage magnitude in p.u.
+  * @param va              Voltage angle in degree
+  * @param maybeSubstation Optional information about the substation assignment
+  * @param subnet          Information about subnet assignment
+  * @param voltLvl         Voltage level information
+  */
+final case class NodePFResult(
+    node: Node,
+    vm: BigDecimal,
+    va: BigDecimal,
+    maybeSubstation: Option[Substation],
+    subnet: String,
+    voltLvl: Int
+) extends EntityModel {
+
+  /**
+    * Identifier
+    */
+  override val id: String = "NO ID NEEDED"
+}
+
+case object NodePFResult extends EntityModelCompanionObject[NodePFResult] {
+  private val NODE: String = "node"
+  private val V_M: String = "vm"
+  private val V_A: String = "va"
+  private val SUBSTATION: String = "substation"
+
+  /**
+    * Get an Array of table fields denoting the mapping to the model's attributes
+    *
+    * @return Array of table headings
+    */
+  override def getFields: Array[HeadLineField] =
+    Array(NODE, V_M, V_A).map(id => MandatoryField(id)) ++ Array[HeadLineField](
+      OptionalField(
+        SUBSTATION
+      )
+    )
+
+  /**
+    * Factory method to build one model from a mapping from field id to value
+    *
+    * @param rawData mapping from field id to value
+    * @return A model
+    */
+  override def apply(rawData: RawModelData): NodePFResult =
+    throw SimbenchDataModelException(
+      s"No basic implementation of model creation available for ${this.getClass.getSimpleName}"
+    )
+
+  /**
+    * Factory method to build a batch of models from a mapping from field id to value
+    *
+    * @param rawData      mapping from field id to value
+    * @param nodes        Nodes to use for mapping
+    * @param substations  Substations to use for mapping
+    * @return A [[Vector]] of models
+    */
+  def buildModels(
+      rawData: Vector[RawModelData],
+      nodes: Map[String, Node],
+      substations: Map[String, Substation]
+  ): Vector[NodePFResult] =
+    for (entry <- rawData) yield {
+      val node = getNode(entry.get(NODE), nodes)
+      val maybeSubstation = substations.get(entry.get(SUBSTATION))
+
+      buildModel(entry, node, maybeSubstation)
+    }
+
+  /**
+    * Factory method to build one model from a mapping from field id to value
+    *
+    * @param rawData         mapping from field id to value
+    * @param node            Node to use
+    * @param maybeSubstation Substation information
+    * @return A model
+    */
+  def buildModel(
+      rawData: RawModelData,
+      node: Node,
+      maybeSubstation: Option[Substation]
+  ): NodePFResult = {
+    val vm = rawData.getBigDecimal(V_M)
+    val va = rawData.getBigDecimal(V_A)
+    val subnet = rawData.get(SUBNET)
+    val voltLvl = rawData.getInt(VOLT_LVL)
+
+    NodePFResult(
+      node,
+      vm,
+      va,
+      maybeSubstation,
+      subnet,
+      voltLvl
+    )
+  }
+}
---
+++ src/test/scala/edu/ie3/simbench/model/NodePFResultSpec.scala
@@ -0,0 +1,94 @@
+package edu.ie3.simbench.model
+
+import edu.ie3.simbench.exception.io.SimbenchDataModelException
+import edu.ie3.simbench.model.datamodel.{Load, NodePFResult, Substation}
+import edu.ie3.simbench.model.datamodel.profiles.LoadProfileType
+import edu.ie3.test.common.{ConverterTestData, UnitSpec}
+
+class NodePFResultSpec extends UnitSpec with ConverterTestData {
+  val nodeMapping = Map(
+    "LV1.101 Bus 1" -> getNodePair("LV1.101 Bus 1")._1
+  )
+
+  val substations = Map(
+    "substation_1" -> Substation("substation_1", "LV1.101", 7)
+  )
+
+  val rawData = Vector(
+    RawModelData(
+      classOf[NodePFResult],
+      Map(
+        "node" -> "LV1.101 Bus 1",
+        "vm" -> "1.04913",
+        "va" -> "-144.292",
+        "substation" -> "substation_1",
+        "subnet" -> "LV1.101",
+        "voltLvl" -> "7"
+      )
+    ),
+    RawModelData(
+      classOf[NodePFResult],
+      Map(
+        "node" -> "LV1.101 Bus 1",
+        "vm" -> "1.04729",
+        "va" -> "-144.168",
+        "substation" -> "NULL",
+        "subnet" -> "LV1.101",
+        "voltLvl" -> "7"
+      )
+    )
+  )
+
+  val expected = Vector(
+    NodePFResult(
+      getNodePair("LV1.101 Bus 1")._1,
+      BigDecimal("1.04913"),
+      BigDecimal("-144.292"),
+      Some(Substation("substation_1", "LV1.101", 7)),
+      "LV1.101",
+      7
+    ),
+    NodePFResult(
+      getNodePair("LV1.101 Bus 1")._1,
+      BigDecimal("1.04729"),
+      BigDecimal("-144.168"),
+      None,
+      "LV1.101",
+      7
+    )
+  )
+
+  "The node result object" should {
+    "throw an exception, when the basic single model creation method is called" in {
+      val thrown =
+        intercept[SimbenchDataModelException](NodePFResult.apply(rawData(0)))
+      thrown.getMessage shouldBe "No basic implementation of model creation available for NodePFResult$"
+    }
+
+    "throw an exception, when the basic batch model creation method is called" in {
+      val thrown =
+        intercept[SimbenchDataModelException](NodePFResult.buildModels(rawData))
+      thrown.getMessage shouldBe "No basic implementation of model creation available for NodePFResult$"
+    }
+
+    "build the correct single model" in {
+      val actual = NodePFResult.buildModel(
+        rawData(0),
+        nodeMapping.getOrElse(
+          "LV1.101 Bus 1",
+          throw SimbenchDataModelException(
+            "Ooops. This is not supposed to happen"
+          )
+        ),
+        substations.get("substation_1")
+      )
+      actual shouldBe expected(0)
+    }
+
+    "build a correct vector of models" in {
+      val actual =
+        NodePFResult.buildModels(rawData, nodeMapping, substations)
+      actual shouldBe expected
+    }
+  }
+}
