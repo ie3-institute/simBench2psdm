587aa6af3a75ec632042c83ab720d40cb0224804
Kittl, Chris <chris.kittl@tu-dortmund.de>
2020-09-03 14:43:24 +0200

    Tidying up the config

--- src/test/scala/edu/ie3/test/common/ConfigTestData.scala
+++ src/test/scala/edu/ie3/test/common/ConfigTestData.scala
@@ -7,7 +7,11 @@ import edu.ie3.simbench.config.SimbenchConfig.Io.Input
 trait ConfigTestData {
   val validIoInputConfig = new SimbenchConfig.Io.Input(
     new SimbenchConfig.CsvConfig("UTF-8", ".csv", ";"),
-    new Input.Download(false, "testData/download/")
+    new Input.Download(
+      "http://141.51.193.167/simbench/gui/usecase/download",
+      false,
+      "testData/download/"
+    )
   )
   val validIoOutputConfig = new SimbenchConfig.Io.Output(
     new config.SimbenchConfig.CsvConfig("UTF-8", ".csv", ";"),
@@ -15,16 +19,12 @@ trait ConfigTestData {
   )
 
   val validIo = new SimbenchConfig.Io(
-    "http://141.51.193.167/simbench/gui/usecase/download",
-    "testData/download/",
     validIoInputConfig,
     validIoOutputConfig,
     List("1-LV-urban6--0-sw", "1-LV-urban6--2-sw", "1-EHVHV-mixed-2-0-sw")
   )
 
   val ioWithInvalidSimbenchCode = new SimbenchConfig.Io(
-    "http://141.51.193.167/simbench/gui/usecase/download",
-    "testData/download/",
     validIoInputConfig,
     validIoOutputConfig,
     List("1-LV-urban6--0-sw", "blabla", "1-EHVHV-mixed-2-0-sw")
