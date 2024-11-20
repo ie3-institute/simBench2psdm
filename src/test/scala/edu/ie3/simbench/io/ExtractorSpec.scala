41f1a6d01326440be383a77b1e3661a9b373282b
PhilippSchmelter <philipp@stressnet.de>
2024-11-20 23:59:41 +0100

    Added Unit Test for Extractor

---
+++ src/test/scala/edu/ie3/simbench/io/ExtractorSpec.scala
@@ -0,0 +1,98 @@
+package edu.ie3.simbench.io
+
+import edu.ie3.simbench.config.SimbenchConfig
+import edu.ie3.simbench.config.SimbenchConfig.Io
+import edu.ie3.simbench.config.SimbenchConfig.Io.{Input, Output}
+import edu.ie3.simbench.config.SimbenchConfig.Io.Input.Download
+
+import java.io.File
+import org.scalatest.matchers.should.Matchers
+import edu.ie3.test.common.UnitSpec
+
+import scala.util.matching.Regex
+
+class ExtractorSpec extends UnitSpec with IoUtils with Matchers {
+  val download: Download = Download(
+    baseUrl = "https://daks.uni-kassel.de/bitstreams",
+    failOnExistingFiles = false,
+    folder = "testData/download"
+  )
+  val input: Input = Input(
+    csv = SimbenchConfig.CsvConfig(
+      directoryHierarchy = false,
+      fileEncoding = "UTF-8",
+      fileEnding = ".csv",
+      separator = ";"
+    ),
+    download = download
+  )
+  val output: Output = Output(
+    compress = true,
+    csv = SimbenchConfig.CsvConfig(
+      directoryHierarchy = false,
+      fileEncoding = "UTF-8",
+      fileEnding = ".csv",
+      separator = ";"
+    ),
+    targetFolder = "convertedData"
+  )
+  val io: Io = Io(input = input, output = output, simbenchCodes = List.empty)
+  val simbenchConfig: SimbenchConfig = SimbenchConfig(
+    conversion = SimbenchConfig.Conversion(removeSwitches = false),
+    io = io
+  )
+
+  val extractor = new Extractor(simbenchConfig)
+  val uuidPattern: Regex =
+    "[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}".r
+
+  "The extraction" should {
+    "download should save the file to the specified location" in {
+      extractor.download()
+      val downloadedFile = new File(
+        s"${simbenchConfig.io.input.download.folder}/simbench_datalinks.csv"
+      )
+      downloadedFile.exists() shouldBe true
+    }
+
+    "extractUUIDMap should return a correct map of codes to UUIDs" in {
+      extractor.download()
+      val uuidMap = extractor.extractUUIDMap()
+      uuidMap should contain key "1-LV-urban6--0-sw"
+      uuidMap should contain key "1-LV-rural1--2-no_sw"
+      uuidMap("1-LV-urban6--0-sw") should fullyMatch regex uuidPattern
+      uuidMap("1-LV-rural1--2-no_sw") should fullyMatch regex uuidPattern
+    }
+
+    "extractUUIDMap should throw an exception if required columns are missing" in {
+      val invalidConfig = simbenchConfig.copy(
+        io = simbenchConfig.io.copy(
+          input = simbenchConfig.io.input.copy(
+            download = simbenchConfig.io.input.download.copy(
+              folder = "invalidFolder"
+            )
+          )
+        )
+      )
+      val invalidExtractor = new Extractor(invalidConfig)
+      an[IllegalArgumentException] should be thrownBy invalidExtractor
+        .extractUUIDMap()
+    }
+
+    "extractUUIDMap should return an empty map if no valid UUIDs are found" in {
+      val emptyConfig = simbenchConfig.copy(
+        io = simbenchConfig.io.copy(
+          input = simbenchConfig.io.input.copy(
+            download = simbenchConfig.io.input.download.copy(
+              folder = "emptyFolder"
+            )
+          )
+        )
+      )
+      val emptyExtractor = new Extractor(emptyConfig)
+      emptyExtractor.download()
+      val uuidMap = emptyExtractor.extractUUIDMap()
+      uuidMap shouldBe empty
+    }
+  }
+}
