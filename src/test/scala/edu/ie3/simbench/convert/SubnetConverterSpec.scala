ee0c2adaf00de4645c1550c819fceb35e17c2168
danielfeismann <daniel.feismann@tu-dortmund.de>
2026-02-04 09:41:13 +0100

    enhance SubnetConverterSpec

--- src/test/scala/edu/ie3/simbench/convert/SubnetConverterSpec.scala
+++ src/test/scala/edu/ie3/simbench/convert/SubnetConverterSpec.scala
@@ -1,5 +1,6 @@
 package edu.ie3.simbench.convert
 
+import edu.ie3.simbench.exception.ConversionException
 import edu.ie3.test.common.UnitSpec
 import org.scalatest.prop.TableDrivenPropertyChecks
 
@@ -12,7 +13,9 @@ class SubnetConverterSpec extends UnitSpec with TableDrivenPropertyChecks {
       (BigDecimal("20"), "MV1"),
       (BigDecimal("10"), "MV"),
       (BigDecimal("0.4"), "LV1"),
-      (BigDecimal("20"), "MV1_LV1")
+      (BigDecimal("20"), "MV1_LV1"),
+      (BigDecimal("110"), "HV1"),
+      (BigDecimal("20"), "MV4.101")
     )
     val converter = SubnetConverter(ratedVoltageIdPairs)
 
@@ -20,11 +23,13 @@ class SubnetConverterSpec extends UnitSpec with TableDrivenPropertyChecks {
       val expectedMapping =
         Map(
           (BigDecimal("380"), "EHV") -> 1,
-          (BigDecimal("20"), "MV1") -> 2,
-          (BigDecimal("20"), "MV2") -> 3,
-          (BigDecimal("10"), "MV") -> 4,
-          (BigDecimal("0.4"), "LV") -> 5,
-          (BigDecimal("0.4"), "LV1") -> 6
+          (BigDecimal("110"), "HV1") -> 2,
+          (BigDecimal("20"), "MV1") -> 3,
+          (BigDecimal("20"), "MV2") -> 4,
+          (BigDecimal("20"), "MV4.101") -> 5,
+          (BigDecimal("10"), "MV") -> 6,
+          (BigDecimal("0.4"), "LV") -> 7,
+          (BigDecimal("0.4"), "LV1") -> 8
         )
 
       val actual = converter.mapping
@@ -42,16 +47,25 @@ class SubnetConverterSpec extends UnitSpec with TableDrivenPropertyChecks {
       val testData = Table(
         ("ratedVoltage", "id", "expected"),
         (BigDecimal("380"), "EHV", 1),
-        (BigDecimal("20"), "MV1", 2),
-        (BigDecimal("0.4"), "LV1", 6),
-        (BigDecimal("20"), "MV1_LV1", 2),
-        (BigDecimal("0.4"), "MV1_LV1", 6)
+        (BigDecimal("20"), "MV1", 3),
+        (BigDecimal("0.4"), "LV1", 8),
+        (BigDecimal("20"), "MV1_LV1", 3),
+        (BigDecimal("0.4"), "MV1_LV1", 8),
+        (BigDecimal("110"), "HV1", 2),
+        (BigDecimal("20"), "MV4.101", 5),
+        (BigDecimal("20"), "MV1_Feeder5", 3)
       )
 
       forAll(testData) {
         (ratedVoltage: BigDecimal, id: String, expected: Int) =>
           converter.convert(ratedVoltage, id) shouldBe expected
       }
+    }
+
+    "throw a ConversionException if the rated voltage does not fit any part of a two-part subnet id" in {
+      val invalidPairs = Vector((BigDecimal("200"), "HV1_LV1"))
+      val thrown = intercept[ConversionException](SubnetConverter(invalidPairs))
+      thrown.getMessage shouldBe "Neither 'HV', nor 'LV' fit the given rated voltage 200 kV."
     }
   }
 }
